9bb3b41354d0c3deffb1ced2e96afae21b218caa
himjjong <jjijji5728@tukorea.ac.kr>
2022-08-10 00:00:09 +0900

    [#2] test

--- your-anmok/src/main/java/com/lion/youranmok/YourAnmokApplication.java
+++ your-anmok/src/main/java/com/lion/youranmok/YourAnmokApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class YourAnmokApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(YourAnmokApplication.class, args);
 	}
 
