6f09e5a7cd0827722e66859531409952aa4e78bc
dajung <ekwjd990913@gmail.com>
2022-08-11 14:20:52 +0900

    [번개] 클릭시 상세 화면 렌더링, card hover시 색깔 일부 수정

--- your-anmok/src/main/java/com/lion/youranmok/gathering/controller/GatheringListController.java
+++ your-anmok/src/main/java/com/lion/youranmok/gathering/controller/GatheringListController.java
@@ -6,6 +6,7 @@ import com.lion.youranmok.gathering.dto.GatheringListDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
@@ -51,17 +52,13 @@ public class GatheringListController {
 
         return "gathering/list";
     }
+
+    @GetMapping("/{boardId}")
+    public String getDetail(@PathVariable int boardId, Model model) {
+        
+        model.addAttribute("bId", boardId);
+        return "gathering/detail";
+    }
 }
 
-//6 - 제 작업
-//develop - 머지하고 싶은 브랜치
-//7 - 아름님 작업
-//
-//6 -> develop에 커밋 했음 push X
-//하고싶은 것: 7->develop (merge)
-
-// 7을 내가 풀 받아서
-
-// 깃허브에 올라온 아름님 브랜치를 develop에 머지 (목표)
-// feautre/#7-popup
 
