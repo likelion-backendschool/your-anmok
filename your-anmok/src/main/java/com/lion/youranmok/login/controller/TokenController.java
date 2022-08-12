810aae4e89a436fd21438360dda8c8fbdfce4bbc
himjjong <jjijji5728@tukorea.ac.kr>
2022-08-12 09:43:33 +0900

    [Login] 카카오 서버에서 Access token val 받아오기

--- your-anmok/src/main/java/com/lion/youranmok/login/controller/TokenController.java
+++ your-anmok/src/main/java/com/lion/youranmok/login/controller/TokenController.java
@@ -1,9 +1,15 @@
 package com.lion.youranmok.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+
+
 
 @Controller
 @RequestMapping("/")
@@ -15,8 +21,34 @@ public class TokenController {
         }
 
     @GetMapping("/auth/kakao/callback")
-    public @ResponseBody
-    String kakaoCallback() {
-        return "카카오 인증 완료";
+    public @ResponseBody String kakaoCallback(String code) {
+        //Post 방식으로 key=value 데이터를 요청(카카오쪽으로)
+        RestTemplate rt = new RestTemplate();
+
+        //httpHeaders 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        //httpBody 오브젝트 생성
+        MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type","authorization_code");
+        params.add("client_id","84bb53ddc4e742b0c6aa6c06a6372dbc");
+        params.add("redirect_uri","http://localhost:8080/auth/kakao/callback");
+        params.add("code",code);
+
+        //httpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
+            new HttpEntity<>(params,headers);
+
+        //Http 요청하기 - Post방식으로 - 그리고 Response변수의 응답 받음.
+        ResponseEntity<String> response = rt.exchange(
+            "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        return "카카오 토큰 요청 완료: 토큰 요청에 대한 응답 "+ response.getBody();
+
     }
 }
