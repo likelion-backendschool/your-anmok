0d70e8e5b87cf476e460dc7e3b0b628ff3518825
Aruming <0430kar@naver.com>
2022-08-12 22:33:12 +0900

    Place 관련 파일 올바른 디렉터리로 이동, 테스트 케이스 작성하여 데이터 한 개 추가

---
+++ your-anmok/src/main/java/com/lion/youranmok/place/dto/PlaceDto.java
@@ -0,0 +1,18 @@
+package com.lion.youranmok.place.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceDto {
+    private int id;
+    private String address;
+    private String name;
+    private int rateCnt;
+    private int star;
+    private double lat;
+    private double lon;
+}
