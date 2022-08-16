f37e31ba661835488f2e45be83d30786da1f609f
ap3334 <chwil2989@naver.com>
2022-08-16 16:00:13 +0900

    페이징 처리

--- your-anmok/src/main/java/com/lion/youranmok/category/controller/CategoryController.java
+++ your-anmok/src/main/java/com/lion/youranmok/category/controller/CategoryController.java
@@ -4,11 +4,13 @@ import com.lion.youranmok.category.dto.CategoryDto;
 import com.lion.youranmok.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +28,12 @@ public class CategoryController {
      * 모든 카테고리 표시됨
      */
     @GetMapping({"", "/home"})
-    public String home(Model model) {
+    public String home(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
 
-        List<CategoryDto> categories = new ArrayList<>();
+//        List<CategoryDto> categories = new ArrayList<>();
 
-        categories = categoryService.findAll();
+        Page<CategoryDto> categories = categoryService.getListByPaging(page);
+        System.out.println(categories.getNumber());
 
         model.addAttribute("categories", categories);
 
