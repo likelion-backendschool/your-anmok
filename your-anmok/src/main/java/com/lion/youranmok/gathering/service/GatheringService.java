e0446b1d9cb70763414c226506dfdec1f728df0e
ap3334 <chwil2989@naver.com>
2022-08-20 16:47:57 +0900

    카테고리 페이지에 보여지는 번개모임 처리 위한 dto, 메서드 생성

--- your-anmok/src/main/java/com/lion/youranmok/gathering/service/GatheringService.java
+++ your-anmok/src/main/java/com/lion/youranmok/gathering/service/GatheringService.java
@@ -2,6 +2,8 @@ package com.lion.youranmok.gathering.service;
 
 import com.lion.youranmok.gathering.dto.GatheringDetailDto;
 import com.lion.youranmok.gathering.dto.GatheringListDetailDto;
+import com.lion.youranmok.gathering.dto.GatheringPreviewDto;
+import com.lion.youranmok.gathering.repository.GatheringCommentRepository;
 import com.lion.youranmok.gathering.repository.GatheringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GatheringService {
     private final GatheringRepository gatheringRepository;
+    private final GatheringCommentRepository gatheringCommentRepository;
 
     public List<GatheringListDetailDto> listByCriteria() {
         return gatheringRepository.listByCriteria();
@@ -21,5 +24,20 @@ public class GatheringService {
         return gatheringRepository.getDetailById(id);
     }
 
+    /**
+     * 카테고리 페이지의 댓글 순으로 나열하기 위한 DTO 가져오는 메서드
+     */
+    public List<GatheringPreviewDto> getPreview() {
+
+        List<GatheringPreviewDto> previewDtos = gatheringRepository.getPreview();
+        previewDtos.forEach(preview -> {
+
+            int cnt = gatheringCommentRepository.getCommentCnt(preview.getBoardId());
+            preview.setCommentCnt(cnt);
+
+        });
+
+        return previewDtos;
+    }
 
 }
