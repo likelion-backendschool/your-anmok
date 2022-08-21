6b785d05f2b17d4e8cde5ff74a7313e819343f45
Aruming <0430kar@naver.com>
2022-08-21 14:04:56 +0900

    카테고리 ê entity, dto 생성

---
+++ your-anmok/src/main/java/com/lion/youranmok/category/dto/CategoryDto.java
@@ -0,0 +1,18 @@
+package com.lion.youranmok.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+
+    private Integer id;
+    private Integer placeId;
+    private String tagName;
+
+}
---
+++ your-anmok/src/main/java/com/lion/youranmok/category/entity/Category.java
@@ -0,0 +1,27 @@
+package com.lion.youranmok.category.entity;
+
+import lombok.*;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.*;
+
+@Data
+@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private Integer placeId;
+
+    @Column(name = "tag_name", nullable = false)
+    private String tagName;
+
+}
