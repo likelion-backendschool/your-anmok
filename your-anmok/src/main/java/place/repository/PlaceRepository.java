580f3e31db4d0df7777a329a9d7befab6c2ca0b6
Aruming <0430kar@naver.com>
2022-08-22 18:12:12 +0900

    파일 삭제 및 오류 수정(#21과 같은 상태로)

--- your-anmok/src/main/java/place/repository/PlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package place.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import place.entity.Place;
-
-@Repository
-public interface PlaceRepository  extends JpaRepository<Place, Integer> {
-}
