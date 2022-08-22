27760bcc587c9e734e6d1676ef9c6a4b5d349d45
Aruming <0430kar@naver.com>
2022-08-22 18:02:07 +0900

    오류 수정

--- your-anmok/src/main/java/com/lion/youranmok/map/controller/HomeController.java
+++ your-anmok/src/main/java/com/lion/youranmok/map/controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import place.dto.PlaceDto;
 
 import java.util.ArrayList;
 import java.util.List;
