c0104b32d58e288b3cd553f82cf73491cab5902e
joonghyun <80939285+joong2043@users.noreply.github.com>
2022-09-05 21:59:51 +0900

    [지도] 등록 버튼 클릭시 DB에 데이터 저장 + 지도에 마커 표시 (#63)
    * feature/#54-map과 development 충돌 수정
    
    * development와 feature/#54-map 병합 수정2

--- your-anmok/src/main/java/com/lion/youranmok/YourAnmokApplication.java
+++ your-anmok/src/main/java/com/lion/youranmok/YourAnmokApplication.java
@@ -2,9 +2,8 @@ package com.lion.youranmok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.lion.youranmok"})
--- your-anmok/src/main/java/com/lion/youranmok/map/controller/MapController.java
+++ your-anmok/src/main/java/com/lion/youranmok/map/controller/MapController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/map")
 @Controller
@@ -20,6 +21,7 @@ public class MapController {
         return "redirect:/";
 
     }
+
 
 
 
