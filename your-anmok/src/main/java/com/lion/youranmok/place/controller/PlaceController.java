bd031d49557e08e354fa7f044573eef9d63e0f4c
Aruming <0430kar@naver.com>
2022-09-12 17:14:14 +0900

    장소추가 POST ã동작하도록 수정

--- your-anmok/src/main/java/com/lion/youranmok/place/controller/PlaceController.java
+++ your-anmok/src/main/java/com/lion/youranmok/place/controller/PlaceController.java
@@ -5,12 +5,15 @@ import com.lion.youranmok.gathering.dto.GatheringPreviewDto;
 import com.lion.youranmok.place.dto.PlaceGatheringDto;
 import com.lion.youranmok.place.dto.PlaceTagDto;
 import com.lion.youranmok.place.entity.Place;
+import com.lion.youranmok.place.service.AddPlaceService;
 import com.lion.youranmok.place.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 
 import java.util.List;
@@ -33,6 +36,19 @@ public class PlaceController {
         model.addAttribute("gatheringList", placeGatheringDtos);
 
         return "map/homeMap";
+    }
+
+    private final AddPlaceService addPlaceService;
+
+    @PostMapping("/addPlace")
+    public String addPlace(String placeName, String address, Integer rating, MultipartFile placeImg) {
+        Place place = placeService.getPlaceByNameAndAddress(placeName, address);
+
+        Integer starAvg = placeService.getStarAvg(place.getId());
+        placeService.setStar(place, starAvg);
+        addPlaceService.upload(place.getId(), rating, placeImg);
+
+        return "redirect:/";
     }
 
 }
