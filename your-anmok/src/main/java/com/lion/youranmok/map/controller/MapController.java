9a9737ae25ac6d0b46e5f33a4f56813473fccaae
joonghyun <80939285+joong2043@users.noreply.github.com>
2022-09-16 00:46:45 +0900

    test/map과 development 충돌 수정1

--- your-anmok/src/main/java/com/lion/youranmok/map/controller/MapController.java
+++ your-anmok/src/main/java/com/lion/youranmok/map/controller/MapController.java
@@ -2,10 +2,14 @@ package com.lion.youranmok.map.controller;
 
 import com.lion.youranmok.map.dto.MapDto;
 import com.lion.youranmok.map.service.MapService;
+import com.lion.youranmok.place.entity.Place;
+import com.lion.youranmok.place.service.AddPlaceService;
+import com.lion.youranmok.place.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -46,6 +50,20 @@ public class MapController {
         model.addAttribute("categoryPlaceList",categoryPlaceList);
 
         return "map/categoryMap";
+    }
+
+    private final AddPlaceService addPlaceService;
+    private final PlaceService placeService;
+
+    @PostMapping("/addPlace")
+    public String addPlace(String placeName, String address, Integer rating, MultipartFile placeImg) {
+        Place place = placeService.getPlaceByNameAndAddress(placeName, address);
+
+        Integer starAvg = placeService.getStarAvg(place.getId());
+        placeService.setStar(place, starAvg);
+        addPlaceService.upload(place.getId(), rating, placeImg);
+
+        return "redirect:/";
     }
 
 }
