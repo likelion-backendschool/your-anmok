c13762f770e3e98525226a657d6119a551b53f95
ap3334 <chwil2989@naver.com>
2022-09-16 10:25:29 +0900

    [#82] [카테고리] 북마크 컨트롤러 로그인한 사용자와 연동

--- your-anmok/src/main/java/com/lion/youranmok/category/controller/BookmarkController.java
+++ your-anmok/src/main/java/com/lion/youranmok/category/controller/BookmarkController.java
@@ -1,7 +1,11 @@
 package com.lion.youranmok.category.controller;
 
 import com.lion.youranmok.category.service.BookmarkService;
+import com.lion.youranmok.security.dto.MemberContext;
+import com.lion.youranmok.user.entity.User;
+import com.lion.youranmok.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +22,18 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
+    private final UserService userService;
+
     @GetMapping("/register")
     public String registerBookmark(@RequestParam("categoryId") int categoryId,
                                    @RequestParam(value = "page", required = false) int page,
                                    @RequestParam(value = "keyword", required = false) String keyword,
-                                   RedirectAttributes redirectAttributes) {
+                                   RedirectAttributes redirectAttributes,
+                                   @AuthenticationPrincipal MemberContext member) {
 
-        bookmarkService.register(1, categoryId);
+        User user = userService.findByUsername(member.getUsername());
+
+        bookmarkService.register(user.getId(), categoryId);
 
         redirectAttributes.addAttribute("keyword", keyword);
         redirectAttributes.addAttribute("page", page);
@@ -35,9 +44,15 @@ public class BookmarkController {
 
     @Transactional
     @GetMapping("/remove")
-    public String removeBookmark(@RequestParam("categoryId") int categoryId, int page, String keyword, RedirectAttributes redirectAttributes) {
+    public String removeBookmark(@RequestParam("categoryId") int categoryId,
+                                 @RequestParam(value = "page", required = false) int page,
+                                 @RequestParam(value = "keyword", required = false) String keyword,
+                                 RedirectAttributes redirectAttributes,
+                                 @AuthenticationPrincipal MemberContext member) {
 
-        bookmarkService.remove(1, categoryId);
+        User user = userService.findByUsername(member.getUsername());
+
+        bookmarkService.remove(user.getId(), categoryId);
 
         redirectAttributes.addAttribute("keyword", keyword);
         redirectAttributes.addAttribute("page", page);
@@ -48,15 +63,11 @@ public class BookmarkController {
 
     @Transactional
     @PostMapping("/removeByMypage")
-    public String removeByMypage(@RequestParam int categoryId) {
+    public String removeByMypage(@RequestParam int categoryId, @AuthenticationPrincipal MemberContext member) {
 
-        System.out.println("BookmarkController.removeByMypage");
+        User user = userService.findByUsername(member.getUsername());
 
-        System.out.println("categoryId = " + categoryId);
-
-        bookmarkService.remove(1, categoryId);
-
-
+        bookmarkService.remove(user.getId(), categoryId);
 
         return "redirect:/mypage";
 
