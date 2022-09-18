a80c59afa40dd39a19cad3b2459f42cd57dd42ed
박다정 <ekwjd990913@gmail.com>
2022-09-19 03:30:42 +0900

    [지도] 충돌해결 및 에러 수정

--- your-anmok/src/main/java/com/lion/youranmok/map/controller/HomeController.java
+++ your-anmok/src/main/java/com/lion/youranmok/map/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.lion.youranmok.map.dto.MapDto;
 import com.lion.youranmok.map.entity.Map;
 import com.lion.youranmok.map.service.MapService;
 import com.lion.youranmok.place.entity.Place;
+import com.lion.youranmok.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,23 @@ public class HomeController {
 
 //    @RequestMapping(value = "/", method = {RequestMethod.GET})
 //    public String home(Model model){
+////        ModelAndView mv = new ModelAndView();
+//
+//        List<Place> placeList = mapService.getAllPlaceList();
+////        for (MapDto mapDto : placeList){
+////            System.out.println(mapDto.getName());
+////        }
+//        model.addAttribute("allPlaceList",placeList);
+////        mv.addObject("placeInfos","placeList");
+////        mv.setViewName("map/homeMap");
+//
+////        model.addAttribute("message", "정상적으로 처리되었습니다.");
+////        model.addAttribute("searchUrl", "https://www.google.com");
+//
+//        return "map/homeMap";
+//    }
+//    @RequestMapping(value = "/", method = {RequestMethod.GET})
+//    public String home(@AuthenticationPrincipal MemberContext member, Model model){
 ////        ModelAndView mv = new ModelAndView();
 //
 //        List<Place> placeList = mapService.getAllPlaceList();
