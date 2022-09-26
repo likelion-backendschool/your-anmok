6ef077d4df14c9c76aa652985c907c0c654427a9
ap3334 <chwil2989@naver.com>
2022-09-26 16:45:07 +0900

    [#81] [카테고리] 카테고리 장소 추가 시 로그인 alert창 띄우기

--- your-anmok/src/main/resources/static/js/category/login.js
+++ your-anmok/src/main/resources/static/js/category/login.js
@@ -12,6 +12,19 @@ let index = {
 
 
         });
+        $("#addPlaceinCategory").on("click", () => {
+
+            var categoryId = $("#searchMapButtonByCategoryId").val();
+
+            if ($(".isLogin").val() == "false") {
+                if (confirm("이 서비스는 로그인을 하셔야 사용 가능합니다. 로그인을 하시겠습니까?")) {
+                    location.href = "https://kauth.kakao.com/oauth/authorize?client_id=84bb53ddc4e742b0c6aa6c06a6372dbc&redirect_uri=http://localhost:8080/auth/kakao/callback&response_type=code";
+                }
+            } else {
+                location.href = "/searchMap?categoryId=" + categoryId;
+            }
+
+        });
     },
 
 };
