be1790a39f1f85f4ab38089b1ddc802e1f485d58
ap3334 <chwil2989@naver.com>
2022-09-27 17:06:53 +0900

    [#81] alert창 디자인 일괄 수정

--- your-anmok/src/main/resources/static/js/category/member.js
+++ your-anmok/src/main/resources/static/js/category/member.js
@@ -1,26 +1,32 @@
-
 let index = {
     init: function () {
         $("#nickname-btn").on("click", () => {
 
+            event.preventDefault();
+
             var newNickname = $("#nickname").val();
 
             if ($.trim($("#nickname").val()) == '') {
-                alert("닉네임을 입력해주세요");
-                location.href = "/mypage";
+
+                alertCustom('닉네임을 입력해주세요');
 
             } else {
-                if(confirm("닉네임을 " + newNickname + "(으)로 변경하시겠습니까?") == true) {
-                    this.changeNickname();
-                }
-                location.href = "/mypage";
 
+                confirmAlertCustom("닉네임을 " + newNickname + "(으)로 변경하시겠습니까?").then((result) => {
+                    if (result == true) {
+                        this.changeNickname();
+                    } else {
+                        swal.close();
+                    }
+                });
             }
 
         });
     },
 
     changeNickname: function () {
+
+        event.preventDefault();
 
         var data = {
             nickname: $("#nickname").val(),
@@ -32,22 +38,78 @@ let index = {
             type: "PUT",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
+            async: false,
             success: function (result) {
 
-                alert("닉네임이 \"" + data.nickname + "\"(으)로 변경되었습니다.");
+                alertCustomTimer("닉네임이 \"" + data.nickname + "\"(으)로 변경되었습니다.");
 
-                location.href = "/mypage";
+                setTimeout(function(){location.href = "/mypage";}, 2000);
+
             },
             error : function (error) {
 
                 console.log(JSON.stringify(error));
 
-                alert("실행이 정상적으로 완료되지 않았습니다. 다시 시도해주세요.")
+                alertCustom("실행이 정상적으로 완료되지 않았습니다. 다시 시도해주세요.");
+
+                setTimeout(function(){location.href = "/mypage";}, 2000);
 
             }
         })
 
     }
+
+
 };
+
+const alertCustom = function (text) {
+
+    swal({
+        text: text,
+    }).then(value => {
+        if (value) {
+            swal.close();
+        }
+    });
+
+};
+
+const alertCustomTimer = function (text) {
+
+    swal({
+        text: text,
+        timer: 30000,
+    }).then(value => {
+        if (value) {
+            swal.close();
+        }
+    });
+
+};
+
+const confirmAlertCustom = function (text) {
+
+    const result = swal({
+        text: text,
+        buttons: {
+            cancel: true,
+            confirm: {
+                text: 'OK',
+                value: 'confirm'
+            },
+        },
+    }).then(value => {
+        if (value == 'confirm') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+
+    return result;
+
+};
+
 
 index.init();
